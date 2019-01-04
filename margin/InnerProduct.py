e809c6d46c38d4024f9cdaab8859f5379bcbbff5
wujiyang <1441615466@qq.com>
2019-01-04 17:01:46 +0800

    inner product

--- margin/InnerProduct.py
+++ margin/InnerProduct.py
@@ -5,16 +5,16 @@
 @contact: wujiyang@hust.edu.cn
 @file: InnerProduct.py
 @time: 2019/1/4 16:54
-@desc: just normal inner product
+@desc: just normal inner product as fully connected layer do.
 '''
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from torch.nn import Parameter
 
-class CosineMarginProduct(nn.Module):
+class InnerProduct(nn.Module):
     def __init__(self, in_feature=128, out_feature=10575):
-        super(CosineMarginProduct, self).__init__()
+        super(InnerProduct, self).__init__()
         self.in_feature = in_feature
         self.out_feature = out_feature
 
