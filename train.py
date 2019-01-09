acddf8c98a30c79aa49987e1bc6500396c759ac7
wujiyang <1441615466@qq.com>
2019-01-09 09:13:14 +0800

    train res50-ir

--- train.py
+++ train.py
@@ -105,7 +105,7 @@ def train(args):
         {'params': net.parameters(), 'weight_decay': 5e-4},
         {'params': margin.parameters(), 'weight_decay': 5e-4}
     ], lr=0.1, momentum=0.9, nesterov=True)
-    exp_lr_scheduler = lr_scheduler.MultiStepLR(optimizer_ft, milestones=[20, 35, 45], gamma=0.1)
+    exp_lr_scheduler = lr_scheduler.MultiStepLR(optimizer_ft, milestones=[10, 20, 30], gamma=0.1)
 
     if multi_gpus:
         net = DataParallel(net).to(device)
@@ -122,7 +122,7 @@ def train(args):
     best_cfp_fp_acc = 0.0
     best_cfp_fp_iters = 0
     total_iters = 0
-    vis = Visualizer(env='arcface_sphere64')
+    vis = Visualizer(env='arcface_Res50_IR')
     for epoch in range(1, args.total_epoch + 1):
         exp_lr_scheduler.step()
         # train model
@@ -227,12 +227,12 @@ if __name__ == '__main__':
     parser.add_argument('--cfpfp_test_root', type=str, default='/media/sda/CFP-FP/cfp_fp_aligned_112', help='agedb image root')
     parser.add_argument('--cfpfp_file_list', type=str, default='/media/sda/CFP-FP/cfp_fp_pair.txt', help='agedb pair file list')
 
-    parser.add_argument('--backbone', type=str, default='SphereNet', help='MobileFace, Res50, Res101, Res50_IR, SERes50_IR, SphereNet')
+    parser.add_argument('--backbone', type=str, default='Res50_IR', help='MobileFace, Res50, Res101, Res50_IR, SERes50_IR, SphereNet')
     parser.add_argument('--margin_type', type=str, default='ArcFace', help='ArcFace, CosFace, SphereFace')
     parser.add_argument('--feature_dim', type=int, default=512, help='feature dimension, 128 or 512')
-    parser.add_argument('--scale_size', type=float, default=64.0, help='scale size')
+    parser.add_argument('--scale_size', type=float, default=32.0, help='scale size')
     parser.add_argument('--batch_size', type=int, default=200, help='batch size')
-    parser.add_argument('--total_epoch', type=int, default=50, help='total epochs')
+    parser.add_argument('--total_epoch', type=int, default=38, help='total epochs')
 
     parser.add_argument('--save_freq', type=int, default=2000, help='save frequency')
     parser.add_argument('--test_freq', type=int, default=2000, help='test frequency')
