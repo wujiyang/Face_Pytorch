47471b83041f236cbdadbbd0f90232d00b4cf124
wujiyang <1441615466@qq.com>
2019-02-15 19:54:50 +0800

    self attention

---
+++ backbone/self_attention.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# encoding: utf-8
+'''
+@author: wujiyang
+@contact: wujiyang@hust.edu.cn
+@file: self_attention.py
+@time: 2019/2/15 19:53
+@desc: self residual attention for deep face recognition, my own paper.
+'''
+
+if __name__ == "__main__":
+    pass
