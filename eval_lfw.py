b9ec0e80517c6be96f6ffbaa8922a8da349c96d3
wujiyang <1441615466@qq.com>
2019-03-20 08:52:15 +0800

    eval lfw

--- eval_lfw.py
+++ eval_lfw.py
@@ -63,18 +63,12 @@ def loadModel(data_root, file_list, backbone_net, gpus='0', resume=None):
 
     if backbone_net == 'MobileFace':
         net = mobilefacenet.MobileFaceNet()
-    elif backbone_net == 'Res50_IR':
-        net = cbam.CBAMResNet_IR(50, feature_dim=args.feature_dim, mode='ir')
-    elif backbone_net == 'SERes50_IR':
-        net = cbam.CBAMResNet_IR(50, feature_dim=args.feature_dim, mode='se_ir')
-    elif backbone_net == 'CBAMRes50_IR':
-        net = cbam.CBAMResNet_IR(50, feature_dim=args.feature_dim, mode='cbam_ir')
-    elif backbone_net == 'Res100_IR':
-        net = cbam.CBAMResNet_IR(100, feature_dim=args.feature_dim, mode='ir')
-    elif backbone_net == 'SERes100_IR':
-        net = cbam.CBAMResNet_IR(100, feature_dim=args.feature_dim, mode='se_ir')
-    elif backbone_net == 'CBAMRes100_IR':
-        net = cbam.CBAMResNet_IR(100, feature_dim=args.feature_dim, mode='cbam_ir')
+    elif backbone_net == 'CBAM_50':
+        net = cbam.CBAMResNet(50, feature_dim=args.feature_dim, mode='ir')
+    elif backbone_net == 'CBAM_50_SE':
+        net = cbam.CBAMResNet(50, feature_dim=args.feature_dim, mode='ir_se')
+    elif backbone_net == 'CBAM_100':
+        net = cbam.CBAMResNet(100, feature_dim=args.feature_dim, mode='ir')
     else:
         print(args.backbone, ' is not available!')
 
@@ -131,11 +125,11 @@ def getFeatureFromTorch(feature_save_dir, net, device, data_set, data_loader):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Testing')
-    parser.add_argument('--root', type=str, default='/media/ramdisk/lfw_align_112', help='The path of lfw data')
-    parser.add_argument('--file_list', type=str, default='/media/ramdisk/pairs.txt', help='The path of lfw data')
-    parser.add_argument('--backbone_net', type=str, default='Res50_IR', help='MobileFace, Res50_IR, SERes50_IR, CBAMRes50_IR, Res100_IR, SERes100_IR, CBAMRes100_IR')
+    parser.add_argument('--root', type=str, default='/media/sda/insightface_emore/lfw_align_112', help='The path of lfw data')
+    parser.add_argument('--file_list', type=str, default='/media/sda/insightface_emore/pair_lfw.txt', help='The path of lfw data')
+    parser.add_argument('--backbone_net', type=str, default='CBAM_100', help='MobileFace, CBAM_50, CBAM_50_SE, CBAM_100')
     parser.add_argument('--feature_dim', type=int, default=512, help='feature dimension')
-    parser.add_argument('--resume', type=str, default='./model/MSCeleb_RES50_IR_20190115_144216/Iter_240000_net.ckpt',
+    parser.add_argument('--resume', type=str, default='./model/InsightFace_ArcFace_CBAM_100_20190319_165001/Iter_003000_net.ckpt',
                         help='The path pf save model')
     parser.add_argument('--feature_save_path', type=str, default='./result/cur_epoch_lfw_result.mat',
                         help='The path of the extract features save, must be .mat file')
